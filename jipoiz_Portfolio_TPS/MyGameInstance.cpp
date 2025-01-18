9d43c79a3a9cfba9b45b7c51280affe2cbcecdf5
jipoiz <143608336+jipoiz@users.noreply.github.com>
2025-01-18 11:22:11 +0900

    Add files via upload
    첫 업로드

---
+++ jipoiz_Portfolio_TPS/MyGameInstance.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+	SchoolName = TEXT("shcool0");
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	UE_LOG(LogTemp, Log, TEXT("%s"), TEXT("Hello Unreal!"));
+
+	UE_LOG(LogTemp, Log, TEXT("-------------------------------"));
+	UClass* ClassRunTime = GetClass();
+	UClass* ClassCompile = UMyGameInstance::StaticClass();
+
+	//검증
+	//check(ClassRunTime == ClassCompile);
+	//ensure(ClassRunTime == ClassCompile);
+	//ensureMsgf(ClassRunTime == ClassCompile, TEXT("error is here"));
+
+	UE_LOG(LogTemp, Log, TEXT("class name = %s"), *ClassRunTime->GetName());
+
+	SchoolName = TEXT("new school name 0");
+
+	UE_LOG(LogTemp, Log, TEXT("%s"), *SchoolName);
+	
+
+	UE_LOG(LogTemp, Log, TEXT("-------------------------------"));
+}
---
+++ jipoiz_Portfolio_TPS/jipoiz_Portfolio_TPS.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "jipoiz_Portfolio_TPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, jipoiz_Portfolio_TPS, "jipoiz_Portfolio_TPS" );
